26478238e89a99ec5c051236738c7f1e82a642ef
Anna Tsarenko <atsarenko@uni-kassel.de>
2024-03-27 00:47:08 +0100

    update some variable names

--- jira-printer/main.py
+++ jira-printer/main.py
@@ -1,4 +1,3 @@
-# This is a sample Python script.
 import csv
 import jinja2
 
@@ -11,25 +10,22 @@ FEATURE_TEMPLATE_FILE = "feature_template.html.j2"
 def read_cards(file_name: str):
     with open(file_name, encoding='utf-8', errors='ignore') as jira_export_file:
         jira_export_csv = csv.reader(jira_export_file)
-        cards = Cards(next(jira_export_csv))
+        jira_cards = Cards(next(jira_export_csv))
         for row in jira_export_csv:
-            cards.add_card(row)
-    return cards
+            jira_cards.add_card(row)
+    return jira_cards
 
 
-def render_cards(cards: list, template: str, name: str):
+def render_cards(jira_cards: list, template: str, name: str):
     template_loader = jinja2.FileSystemLoader(searchpath="templates/")
     template_env = jinja2.Environment(loader=template_loader)
     template = template_env.get_template(template)
-    output = template.render(cards=cards)
+    output = template.render(cards=jira_cards)
     with open("output/" + name + ".html", "w") as f:
         f.write(output)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     cards = read_cards("input/tasks_full.csv")
     render_cards(cards.story_cards, STORY_TEMPLATE_FILE, "stories")
     render_cards(cards.feature_cards, FEATURE_TEMPLATE_FILE, "epics")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
